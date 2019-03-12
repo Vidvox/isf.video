94c8f43bfb724ba04d7f5b2092b2fa585c4902f7
jackturner <jack@jackturner.design>
2019-03-12 17:31:18 -0400

    Close #17. Progress on #11.

--- src/js/isf.js
+++ src/js/isf.js
@@ -2,3 +2,12 @@ $('.hamburger').click(function(){
   $(this).add('#nav nav').toggleClass('open');
   $('html').toggleClass('noscroll')
 });
+
+// const title = document.querySelector('.parallax-container img');
+// const speed = 0.2;
+
+// title.style.transform = 'translateY( calc( var(--scrollparallax) * -1px ) )';
+
+// window.addEventListener('scroll', function() {
+//   title.style.setProperty('--scrollparallax', (document.body.scrollTop || document.documentElement.scrollTop) * speed);
+// });
